0b47854b0f36b9a3d69936aaff7e935d384a76ec
James Chartrand <jc.chartrand@gmail.com>
2025-01-10 10:41:58 -0500

    add deep equal test on positive result

--- test/Verify.spec.ts
+++ test/Verify.spec.ts
@@ -1,17 +1,54 @@
-import { expect } from 'chai'
+import chai from 'chai'
+import deepEqualInAnyOrder from 'deep-equal-in-any-order'
 import { strict as assert } from 'assert';
 import { verifyCredential } from '../src/Verify'
 import { getVCv2Expired, getVCv1Tampered, getVCv2ValidStatus } from '../src/test-fixtures/vc'
 import { knownDIDRegistries } from '../.knownDidRegistries';
 
+chai.use(deepEqualInAnyOrder);
+const {expect} = chai;
+
+const expectedResult = {
+  "verified": true,
+  "log": [
+    {
+      "id": "valid_signature",
+      "valid": true
+    },
+    {
+      "id": "issuer_did_resolves",
+      "valid": true
+    },
+    {
+      "id": "expiration",
+      "valid": true
+    },
+    {
+      "id": "revocation_status",
+      "valid": true
+    },
+    {
+      "id": "registered_issuer",
+      "valid": true,
+      "foundInRegistries": [
+        "DCC Sandbox Registry"
+      ]
+    }
+  ],
+  credential: null,
+  "isFatal": false
+}
+
 describe('Verify', () => {
-  it('verifies with valid status', async () => {
+  it.only('verifies with valid status', async () => {
     const credential : any = getVCv2ValidStatus()
+    expectedResult.credential = credential;
     const result = await verifyCredential({credential, reloadIssuerRegistry: true, knownDIDRegistries})
     console.log("result returned from verifyCredential call:")
     console.log(JSON.stringify(result,null,2))
     expect(result.verified).to.be.true
     expect(result.credential).to.eql(credential)
+    expect(result).to.deep.equalInAnyOrder(expectedResult) // eslint-disable-line no-use-before-define
    })
 
    it('returns fatal error when tampered', async () => {
