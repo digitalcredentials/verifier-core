1e2360e34e35746947dc872c4eb46e8be19c6aa8
James Chartrand <jc.chartrand@gmail.com>
2025-01-29 15:38:39 -0500

    remove old status list check

--- src/credentialStatus.ts
+++ src/credentialStatus.ts
@@ -1,5 +1,4 @@
 import { checkStatus } from '@digitalcredentials/vc-bitstring-status-list';
-import { checkStatus as checkStatusLegacy } from '@digitalcredentials/vc-status-list';
 import { Credential } from './types/credential';
 
 export enum StatusPurpose {
@@ -8,21 +7,18 @@ export enum StatusPurpose {
 }
 
 export function getCredentialStatusChecker(credential: Credential) : (() => boolean) | null  {
+  let statusChecker = null;
   if (!credential.credentialStatus) {
     return null;
   }
-
   const credentialStatuses = Array.isArray(credential.credentialStatus) ?
     credential.credentialStatus :
     [credential.credentialStatus];
   const [credentialStatus] = credentialStatuses;
-  switch (credentialStatus.type) {
-  case 'BitstringStatusListEntry':
-    return checkStatus;
-  case 'StatusList2021Entry':
-    return checkStatusLegacy;
-  default:
-    return null;
+  if (credentialStatus.type === 'BitstringStatusListEntry') {
+    statusChecker = checkStatus;
   }
+  return statusChecker;
+  
 }
 
