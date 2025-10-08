3b44004e5b39442c62b7047d78af127c96f2117e
James Chartrand <jc.chartrand@gmail.com>
2025-10-08 15:19:53 -0400

    fix errors with ajv formatting and schema version

--- src/schemaCheck.ts
+++ src/schemaCheck.ts
@@ -1,20 +1,22 @@
-import { Ajv } from "ajv"
+import { Ajv2019 } from "ajv/dist/2019.js"
+import addFormats from "ajv-formats";
 import { Credential } from "./types/credential.js"
 
-export const checkSchemas = async (vc: Credential) : Promise<any> => {
+
+
+export const checkSchemas = async (vc: Credential): Promise<any> => {
   try {
-    const ajv = new Ajv({ allErrors: true }) // options can be passed, e.g. {allErrors: true}
     if (vc.credentialSchema) {
       // wrap in array if not already
       const credentialSchemas = [].concat(vc.credentialSchema as any);
       const results = await Promise.all(credentialSchemas.map(async (credentialSchema: any) => {
-        const schemaURL = credentialSchema.id
-        const schema = await fetchSchema(schemaURL);
-        console.log("THE SCHEMA:")
-        console.log(schema)
+        const schema = await fetchSchema(credentialSchema.id);
+        const ajv = new Ajv2019({ allErrors: true })
+        addFormats.default(ajv);
         const validate = ajv.compile(schema)
         const valid = validate(vc)
-        return valid ? { valid } : { valid, errors: validate.errors }
+        const verificationResult = { valid, ...(validate.errors && { errors: validate.errors }) }
+        return verificationResult
       }));
       return results;
     } else {
@@ -27,7 +29,7 @@ export const checkSchemas = async (vc: Credential) : Promise<any> => {
   }
 }
 
-async function fetchSchema(url: string) : Promise<object> {
+async function fetchSchema(url: string): Promise<object> {
   try {
     const response = await fetch(url);
     const data = await response.json();
