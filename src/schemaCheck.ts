f6b9e002f8607025b739a363579782e3428b182b
James Chartrand <jc.chartrand@gmail.com>
2025-10-09 09:44:23 -0400

    add obv3 schema check based on context

--- src/schemaCheck.ts
+++ src/schemaCheck.ts
@@ -2,21 +2,28 @@ import { Ajv2019 } from "ajv/dist/2019.js"
 import addFormats from "ajv-formats";
 import { Credential, CredentialSchema } from "./types/credential.js"
 
+const OBv3_0_3_CONTEXT = 'https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json';
+const OBV3_0_3_SCHEMA = 'https://purl.imsglobal.org/spec/ob/v3p0/schema/json/ob_v3p0_achievementcredential_schema.json'
 const ajv = new Ajv2019({ allErrors: true, loadSchema })
 addFormats.default(ajv);
 
 export const checkSchemas = async (vc: Credential): Promise<any> => {
   try {
     if (vc.credentialSchema) {
-      // wrap in array if not already
+      // wrap all schemas in array if not already
       const credentialSchemas = [].concat(vc.credentialSchema as any);
       const results = await Promise.all(credentialSchemas.map(async (credentialSchema: any) => {
         return await validate(credentialSchema.id, vc);
       }));
       return results;
     } else {
-      // will check for OBv3 in context,
-      // and use it's schema even though it's not specified in vc.credentialSchema
+      // no credentialSchema was specified so try to guess based on context
+      // TODO make sure to indicate in results that this is a guess based on context                        
+      if (vc["@context"].includes(OBv3_0_3_CONTEXT)) {
+        return [await validate(OBV3_0_3_SCHEMA, vc)];
+      } else {
+        return {result: 'NO_SCHEMA'}
+      }
     }
 
   } catch (e) {
