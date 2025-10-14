b52e6175aca69f3b523a73aacf822e54186ed0ab
James Chartrand <jc.chartrand@gmail.com>
2025-10-14 15:00:18 -0400

    add failing schema test

--- test/Verify.schema.spec.ts
+++ test/Verify.schema.spec.ts
@@ -3,6 +3,7 @@ import { verifyCredential } from '../src/Verify.js'
 import { Credential } from '../src/types/credential.js';
 import { knownDIDRegistries } from '../src/test-fixtures/knownDidRegistries.js';
 import { checkSchemas } from '../src/schemaCheck.js';
+import { SCHEMA_ENTRY_ID } from '../src/constants/verificationSteps.js';
 const { expect } = chai;
 
 /* 
@@ -10,63 +11,74 @@ Tests credential *schema* validation.
 */
 
 describe('schemaCheck.checkSchemas', () => {
-    it.only('passes for array proof', async () => {
+    it('passes for array proof', async () => {
         const originalVC = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/dataIntegrityProof/didKey/legacyRegistry-noStatus-notExpired-withSchema.json')
         const vc = JSON.parse(JSON.stringify(originalVC))
         vc.proof = [vc.proof]
         const result = await checkSchemas(vc)
-        // const result = await verifyCredential({ credential: didKeyCredential, knownDIDRegistries })
-        console.log(JSON.stringify(result, null, 2))
         expect(result.results[0].result.errors).to.not.exist
         expect(result.results[0].result.valid).to.be.true
     })
 
-    it.only('fails for object proof', async () => {
+    it('fails for object proof', async () => {
         const vc = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/dataIntegrityProof/didKey/legacyRegistry-noStatus-notExpired-withSchema.json')
         const result = await checkSchemas(vc)
         expect(result.results[0].result.errors).to.exist
         expect(result.results[0].result.valid).to.be.false
     })
 
-    it.only('returns NO_SCHEMA when no credentialSchema property or context', async () => {
+    it('returns NO_SCHEMA when no credentialSchema property or context', async () => {
         const vc = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/dataIntegrityProof/didKey/legacyRegistry-noStatus-noExpiry-minimal.json')
         const result = await checkSchemas(vc)
-        // const result = await verifyCredential({ credential: didKeyCredential, knownDIDRegistries })
-        console.log(JSON.stringify(result, null, 2))
         expect(result).to.deep.equalInAnyOrder({ results: 'NO_SCHEMA' })
     })
 
-    it.only('fails for obv3 based on context with object proof', async () => {
+    it('fails for obv3 based on context with object proof', async () => {
         const vc = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didKey/legacy-noStatus-noExpiry.json')
         const result = await checkSchemas(vc)
-        // const result = await verifyCredential({ credential: didKeyCredential, knownDIDRegistries })
         console.log(JSON.stringify(result, null, 2))
         expect(result.results[0].result.errors).to.exist
         expect(result.results[0].result.valid).to.be.false
     })
 
-    it.only('passes for obv3 based on context with array proof', async () => {
+    it('passes for obv3 based on context with array proof', async () => {
         const originalVC = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didKey/legacy-noStatus-noExpiry.json')
         const vc = JSON.parse(JSON.stringify(originalVC))
         vc.proof = [vc.proof]
         const result = await checkSchemas(vc)
-        // const result = await verifyCredential({ credential: didKeyCredential, knownDIDRegistries })
         expect(result.results[0].result.errors).to.not.exist
         expect(result.results[0].result.valid).to.be.true
     })
 })
 
 describe('schema results for verification call', () => {
-    it.only('returns positive result', async () => {
+    it('returns positive result', async () => {
         const originalVC = await fetchVC('https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didKey/legacy-noStatus-noExpiry.json')
         const credential = JSON.parse(JSON.stringify(originalVC))
         credential.proof = [credential.proof]
         const result = await verifyCredential({ credential, knownDIDRegistries })
         console.log("result: ", JSON.stringify(result,null,2))
-        expect(result).to.exist
-        // need to change these to get pull the schema result from the VC result:
-        // expect(result[0].errors).to.not.exist
-        //  expect(result[0].valid).to.be.true
+        expect(result.additionalInformation![0].id).to.equal(SCHEMA_ENTRY_ID);
+        expect(result.additionalInformation![0].results![0].result.valid).to.be.true
+        expect(result.additionalInformation![0].results![0].source).to.equal("Assumed based on vc.type: 'OpenBadgeCredential' and vc version: 'version 2'")
+    })
+
+    it.only('returns error for extra term', async () => {
+        const credential = await fetchVC("https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/dataIntegrityProof/didKey/twoOIDF-revoked-notExpired-badSchema.json");
+        const result = await verifyCredential({ credential, knownDIDRegistries })
+        console.log("result: ", JSON.stringify(result,null,2))
+        expect(result.additionalInformation![0].id).to.equal(SCHEMA_ENTRY_ID)
+        expect(result.additionalInformation![0].results![0].result.valid).to.be.false
+        expect(result.additionalInformation![0].results![0].source).to.equal("Assumed based on vc.type: 'OpenBadgeCredential' and vc version: 'version 2'")
+        expect(result.additionalInformation![0].results![0].result.errors![0]).to.deep.equalInAnyOrder({
+                "instancePath": "",
+                "schemaPath": "#/required",
+                "keyword": "required",
+                "params": {
+                  "missingProperty": "validFrom"
+                },
+                "message": "must have required property 'validFrom'"
+              })
     })
 })
 
